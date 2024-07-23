11a98884676030375f1801d141338ec93a6f1929
Alessandro Souza <alessandrojsouza@gmail.com>
2024-07-23 09:46:56 -0300

    versão inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'angular-rotas';
+export class AppComponent implements OnInit{
+  filtro = '';
+
+  linguagens = [
+    { id: 1, nome: 'Java', tipo: 'Compilada' },
+    { id: 2, nome: 'Python', tipo: 'Interpretada' },
+    { id: 3, nome: 'JavaScript', tipo: 'Interpretada' },
+    { id: 4, nome: 'C#', tipo: 'Compilada' },
+  ];
+
+  constructor(
+    private acRouter: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.acRouter.queryParams.subscribe(params => {
+      const param = params['filtro'] || '';
+      console.log(param);
+      this.linguagens = this.linguagens.filter(l => l.nome.toLowerCase().includes(param.toLowerCase()));
+    });
+
+  }
+
+  filtrar() {
+    const queryParams = {
+      filtro: this.filtro,
+    };
+    this.router.navigate(['/pesquisar'], { queryParams });
+  }
+
 }
